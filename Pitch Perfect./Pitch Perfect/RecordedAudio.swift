eeab0f160a8aa038324becc5760b9c22c5fe0642
ariff20 <ariffsyed10@gmail.com>
2015-04-24 17:54:59 +0800

    Committed by Udacity

---
+++ Pitch Perfect./Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Sharifah Nazreen Ashraff ali on 3/23/15.
+//  Copyright (c) 2015 Syed Ariff. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
